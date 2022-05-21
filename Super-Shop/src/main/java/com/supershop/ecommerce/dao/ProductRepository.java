ccdf03006f2997497f47f0a4922a0f855c3e9c94
Mudar Hussain <v448474@gmail.com>
2022-05-21 00:35:01 -0400

    Delete Super-Shop directory

--- Super-Shop/src/main/java/com/supershop/ecommerce/dao/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.supershop.ecommerce.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.supershop.ecommerce.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-
-}
